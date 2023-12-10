329c3d04f3fc3410a41cf8cdafe99193919fe8c4
nicholasb4711 <nicholasb4711@gmail.com>
2023-12-09 21:46:46 -0500

    added userlist route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import { useState } from "react";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {DataLayer} from './DataLayer';
+import { DataLayer } from './DataLayer';
 import reducer, { initialState } from './reducer';
+import UserList from "./users/list";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DataLayer initialState = {initialState} reducer = {reducer}>
-      <App />
-    </ DataLayer>
+    <BrowserRouter>
+      <DataLayer initialState={initialState} reducer={reducer}>
+        <App />
+      </ DataLayer>
+      <Routes>
+        <Route path="/users" element={<UserList />} />
+
+      </Routes></BrowserRouter>
+
   </React.StrictMode>
 );
 
